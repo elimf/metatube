39a32b7c1f1440dcefe53d052c7339642d16ee19
Elim Florvil <elim.florvil@ceo-vision.com>
2023-11-28 14:24:09 +0100

    Add login page styles and functionality

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import './login.css';
 
-const login = () => {
+const Login = () => {
   return (
-    <div>page</div>
-  )
+    <>
+      <div className="container">
+        <div className="top-content">
+          <Image src="/metatube.png" alt="" width="100" height="100" />
+          <h2>Sign in</h2>
+          <p className="heading">Use your Google Account</p>
+        </div>
+        <div className="inputs">
+          <input type="email" name="" id="email" className="input" />
+          <label htmlFor="email" className="input-label">Email or phone</label>
+        </div>
+        <a href="" className="link-btn">Forgot Email?</a>
+        <p className="color">Not your computer? Use Guest mode to sign in privately.</p>
+        <a href="" className="link-btn">Learn More</a>
+        <div className="btn-group">
+          <button className="create-btn">Create account</button>
+          <button className="next-btn">Next</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default login
+export default Login;
