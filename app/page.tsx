e41fe7d2d2215dc6e074cff479e98f06a1ef00a6
Elim Florvil <90334513+elimf@users.noreply.github.com>
2023-12-10 15:29:39 +0100

    Refactor gallery layout in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -5,22 +5,24 @@ export default function Home() {
   return (
     <>
       <Header>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {galleryItems.map((item) => (
-            <div key={item} className="overflow-hidden bg-gray-300">
-              <Image
-                src={`https://via.placeholder.com/400`}
-                alt={`Gallery Item ${item}`}
-                className="object-cover w-full h-48"
-                width={400}
-                height={400}
-              />
-              <div className="p-4">
-                <p className="text-lg font-bold">{`Gallery Item ${item}`}</p>
-                <p className="text-gray-500">Description or caption here.</p>
+        <div className="flex-grow">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4">
+            {galleryItems.map((item) => (
+              <div key={item} className="overflow-hidden bg-gray-300">
+                <Image
+                  src={`https://via.placeholder.com/400`}
+                  alt={`Gallery Item ${item}`}
+                  className="object-cover w-full h-48"
+                  width={400}
+                  height={400}
+                />
+                <div className="p-4">
+                  <p className="text-lg font-bold">{`Gallery Item ${item}`}</p>
+                  <p className="text-gray-500">Description or caption here.</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </Header>
     </>
