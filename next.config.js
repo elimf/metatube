beb681ea2ebd2b64175366b41bfc4afbb135bc37
Elim Florvil <90334513+elimf@users.noreply.github.com>
2023-12-10 18:07:04 +0100

    Add publicRuntimeConfig for API_URL

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
 module.exports = {
+  publicRuntimeConfig: {
+    API_URL: process.env.API_URL || "http://localhost:1337",
+  },
   images: {
     remotePatterns: [
       {
