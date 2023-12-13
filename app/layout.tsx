7e49840f860e647e0849a09581f6fdc089de7a7f
Elim Florvil <90334513+elimf@users.noreply.github.com>
2023-12-13 03:38:07 +0100

    Add ToastContainer to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { ToastContainer, toast } from "react-toastify";
 import "@/styles/globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +14,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <>
+          {children} <ToastContainer />
+        </>
+      </body>
     </html>
   );
 }
