32bb6eefe3cc33656dfe01981acf21aa484c46f9
elimf <90334513+elimf@users.noreply.github.com>
2024-05-27 09:01:32 +0200

    Update page.tsx

--- app/playlist/page.tsx
+++ app/playlist/page.tsx
@@ -1,19 +1,28 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import Header from "@/components/Header/Header";
 import { useSearchParams } from "next/navigation";
 import withAuth from "@/utils/authManager";
 
-const Playlist: React.FC = () => {
+const PlaylistContent: React.FC = () => {
   const searchParams = useSearchParams();
   const list = searchParams.get("list");
 
   return (
     <Header withSidebar={true}>
       <div className="flex-grow mt-24 px-8">
-        <h1>Playlist : {list}</h1>
+        <h1>Playlist: {list}</h1>
       </div>
     </Header>
   );
 };
+
+const Playlist: React.FC = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <PlaylistContent />
+    </Suspense>
+  );
+};
+
 export default withAuth(Playlist);
